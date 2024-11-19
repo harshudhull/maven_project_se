f8e69bd1495d1badf1fd6ae3cd6e9aed862e91c3
Harshu <harshudhull46@gmail.com>
2024-11-19 15:37:07 +0530

    maven_java

---
+++ src/main/java/MAVEN_JAVA/au/App.java
@@ -0,0 +1,10 @@
+package MAVEN_JAVA.au;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/MAVEN_JAVA/au/AppTest.java
@@ -0,0 +1,19 @@
+package MAVEN_JAVA.au;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
